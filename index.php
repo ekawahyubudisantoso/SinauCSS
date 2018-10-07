ae8dc82f23dc5a691abe540e08e0fd9d3e3b47fe
ekawahyubudisantoso313 <mbahbudi313@gmail.com>
2018-10-07 14:30:42 +0700

    Ini idexnya vroh

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <title>Belajar HTML</title>
+  </head>
+  <body>
+    <div id="atas">
+      <h1>Sinau Ngoding</h1>
+    </div>
+    <div id="html">
+      <h2>The html5</h2>
+      <img src="image/logo-html.png" alt="HTML">
+    </div>
+    <div id="atas2">
+      <h2>About Us</h2>
+      <h3>"Sinau Ngoding adalah situs web developer informasi, dengan tutorial dan referensi yang berkaitan dengan topik pengembangan web seperti html, css, JavaScript.</h3>
+    </div>
+    <div id="css">
+        <h2>The css3</h2>
+        <img src="image/logo-css.png" alt="CSS">
+    </div>
+    <div id="bawah">
+    </div>
+    <div id="js">
+      <h2>The js</h2>
+      <img src="image/logo-js.png">
+    </div>
+    <div id="bawah2">
+    </div>
+  </body>
+</html>
